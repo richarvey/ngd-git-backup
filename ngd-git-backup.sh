f9effd52c6d5c5e38ce206b8dc23a2c815589bf8
Dan Robertson <daniel@ngineered.co.uk>
2016-08-23 10:27:05 +0100

    limit set to 200, optional 4th arg to set this higher/lower

--- ngd-git-backup.sh
+++ ngd-git-backup.sh
@@ -3,14 +3,19 @@
 ORGANISATION=$1
 TOKEN=$2
 WORK_DIR="$3/${ORGANISATION}"
+PROJECT_LIMIT=$4
+
+if [ -z "$4" ]; then
+  PROJECT_LIMIT=200
+fi
 
 # Pull list of repositories from GitHub API
-REPO_LIST=$(curl -i https://api.github.com/orgs/$ORGANISATION/repos?access_token=$TOKEN'&per_page=200' | grep clone_url)
+REPO_LIST=$(curl -i https://api.github.com/orgs/$ORGANISATION/repos?access_token=$TOKEN'&per_page='$PROJECT_LIMIT | grep clone_url)
 
 # Create workdir if it doesnt exist
 if [ ! -d $WORK_DIR ]; then
   mkdir -p $WORK_DIR
-fi 
+fi
 
 # Change into working directory
 if [ ! -z $WORK_DIR ]; then
@@ -27,13 +32,13 @@ for REPO in $REPO_LIST; do
       REPO=$(echo $REPO | awk -F 'https://' '{print $2} ' )
       echo "Cloning $PROJECT"
       CLONE_URL=$(echo "https://"$TOKEN"@"$REPO)
-      git clone $CLONE_URL 
+      git clone $CLONE_URL
     # Pull / update if the project does exist locally
     elif [ -d $PROJECT ]; then
       cd $PROJECT
       echo "Updating $PROJECT"
       git pull
       cd $WORK_DIR
-    fi	
-  fi	
+    fi
+  fi
 done
