6e34c7fd09378514284124a1f0009e8efa7cb872
ash <ash.github@comtek.co.uk>
2018-04-09 20:24:38 +0100

    Rewrote parsing a little. Errors now go to stderr

--- ngd-git-backup.sh
+++ ngd-git-backup.sh
@@ -6,39 +6,51 @@ WORK_DIR="$3/${ORGANISATION}"
 PROJECT_LIMIT=$4
 
 if [ -z "$4" ]; then
-  PROJECT_LIMIT=200
+	PROJECT_LIMIT=200
 fi
 
 # Pull list of repositories from GitHub API
-REPO_LIST=$(curl -i https://api.github.com/orgs/$ORGANISATION/repos?access_token=$TOKEN'&per_page='$PROJECT_LIMIT | grep clone_url)
+CURL_OUTPUT=$(curl --silent --show-error --fail -i https://api.github.com/orgs/$ORGANISATION/repos?access_token=$TOKEN'&per_page='$PROJECT_LIMIT)
+if [ "$?" != 0 ]
+then
+	echo "Error fetching repo list from GitHub API $CURL_OUTPUT">&2
+	exit 1
+fi
+REPO_LIST=$(echo "$CURL_OUTPUT" | grep clone_url)
 
 # Create workdir if it doesnt exist
 if [ ! -d $WORK_DIR ]; then
-  mkdir -p $WORK_DIR
+	mkdir -p $WORK_DIR
 fi
 
 # Change into working directory
 if [ ! -z $WORK_DIR ]; then
-  cd $WORK_DIR
+	cd $WORK_DIR
 fi
 
-for REPO in $REPO_LIST; do
-  if [[ $REPO != "\"clone_url\":" ]]; then
-    REPO=$(echo $REPO | tr -d "\"" | tr -d ",")
-    PROJECT=$(echo $REPO | awk -F '/' '{print $5} ' | awk -F '.' '{print $1} ')
-    # Clone if the project doesn't exisit locally
-    if [ ! -d $PROJECT ]; then
-      # cut https:// off to insert <token>@
-      REPO=$(echo $REPO | awk -F 'https://' '{print $2} ' )
-      echo "Cloning $PROJECT"
-      CLONE_URL=$(echo "https://"$TOKEN"@"$REPO)
-      git clone $CLONE_URL
-    # Pull / update if the project does exist locally
-    elif [ -d $PROJECT ]; then
-      cd $PROJECT
-      echo "Updating $PROJECT"
-      git pull
-      cd $WORK_DIR
-    fi
-  fi
-done
+echo "$CURL_OUTPUT"|
+	awk "-vtoken=$TOKEN" '
+/^ *"clone_url": ".*",$/{
+	gsub(/^ *"clone_url": "/, "");
+	gsub(/",$/, "");
+	repo=clone_url=$0
+	gsub(/^.*[/]/, "", repo);
+	gsub(/.git$/, "", repo);
+	gsub(/^https:[/][/]/, "https://" token "@", clone_url);
+	printf("%s\t%s\t%s\n", $0, repo, clone_url)
+}'|
+	while IFS=$'\t' read -r REPO PROJECT CLONE_URL; do
+		echo Processing $REPO
+		# Clone if the project doesn't exist locally
+		if [ ! -d $PROJECT ]; then
+		# Pull / update if the project does exist locally
+			echo "Cloning $PROJECT"
+			git clone $CLONE_URL || echo "Errors encountered while cloning $PROJECT">&2
+		elif [ -d $PROJECT ]; then
+			echo "Updating $PROJECT"
+			(
+				cd $PROJECT
+				git pull || echo "Errors encountered while pulling $PROJECT">&2
+			)
+		fi
+	done
